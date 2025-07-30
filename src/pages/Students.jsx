7c7e9e5ccb9ac9ae353bdb16f0184d2a1661e3f7
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-30 10:31:25 +0500

    update commit

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import StudentsCard from "../components/StudentsCard"
 
 export const studentsList = [
@@ -11,6 +12,11 @@ export const studentsList = [
 const Students = () => {
   return (
     <div className="flex justify-center mx-auto gap-5 py-10 flex-wrap w-[80%]">
+       <Link to="/" className="border-none absolute right-12 ">
+        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+        </svg>
+      </Link>
       {studentsList.map(item => <StudentsCard key={item.id} item={item}/>)}
     </div>
   )
